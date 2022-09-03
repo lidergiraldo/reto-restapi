be8cc002bf550814ce53311fb14591e79c9fd509
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-03 01:37:59 -0500

    First version branch admin/listing-product

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,8 +2,10 @@ const { Router } = require('express')
 const router = Router()
 const _ = require('underscore')
 
+const product = require('../Productos.json')
+
 router.get('/producto', (request, response) => {
-    response.json({"t":"t"})
+    response.json(product)
 })
 
 module.exports = router
