cedacade07e268efeef7e284ec10496970efbfc0
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-03 10:58:42 -0500

    Endpoint consult-product- finished

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
+const { result } = require('underscore')
 const router = Router()
 const _ = require('underscore')
 
-router.get('/producto', (request, response) => {
-    response.json({"t":"t"})
+const products = require('../Productos.json')
+
+router.get('/producto/:sku', (request, response) => {
+    const { sku } = request.params
+    
+    const result = products.filter((element) => element.sku == sku)
+    response.json(result)
+     
 })
 
 module.exports = router
