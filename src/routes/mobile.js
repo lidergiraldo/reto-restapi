0402442111926afaaae26f44003f5f237c57cd2e
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-04 15:45:13 -0500

    Endpoint Mobile-summary-purchase- finished

--- src/routes/mobile.js
+++ src/routes/mobile.js
@@ -2,8 +2,35 @@ const { Router } = require('express')
 const router = Router()
 const _ = require('underscore')
 
-router.get('/producto', (request, response) => {
-    response.json({"s":"s"})
+const products = require('../Productos.json')
+
+router.post('/resumen', (request, response) => {
+    const { array_prod } = request.body
+
+    let array_result = []
+    let obj_produ = {}
+    let valor_total = 0
+    _.each(array_prod, (array_p, i) => {
+        let search_prod = products.filter((element) =>{
+            if(element.sku == array_prod[i]){
+                let sku = element.sku
+                let nombre = element.nombre
+                let precio = element.precio
+                let descuento = element.descuento
+                let iva = element.iva
+
+                let precio_final_product = precio - (precio * descuento) + (precio * iva)
+                valor_total += precio_final_product
+
+                obj_produ = { sku, nombre, precio_final_product }
+            }
+        })
+        array_result.push(obj_produ)
+    })
+    const showResume = { valor_total }
+    array_result.push(valor_total)
+
+    response.json(array_result)
 })
 
 module.exports = router
