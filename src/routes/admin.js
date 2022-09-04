9481bdb51684ebd3f8fffd421356f26d0e826db1
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-03 20:36:11 -0500

    Endpoint delete-product- finished

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,28 @@
 const { Router } = require('express')
 const router = Router()
 const _ = require('underscore')
+const fs = require('fs')
 
-router.get('/producto', (request, response) => {
-    response.json({"t":"t"})
+const products = require('../Productos.json')
+
+router.delete('/producto/:sku', (request, response) => {
+    const { sku } = request.params;
+
+    _.each(products, (product, i) => {
+        if(product.sku == sku){
+            products.splice(i, 1)
+        }
+    })
+
+    const delete_process = JSON.stringify(products)
+    fs.writeFileSync('src/Productos.json', delete_process, (error) => {
+        if(error){
+            console.log(`Error: ${error}`)
+        }
+    })
+
+    response.json('Product successfully removed')
+
 })
 
 module.exports = router
