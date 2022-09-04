7f8c7a3333ce96a07c05c0fdd2bc215b08ece8a7
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-04 00:47:13 -0500

    Endpoint Mobile listing-products- finished

--- src/routes/mobile.js
+++ src/routes/mobile.js
@@ -2,8 +2,30 @@ const { Router } = require('express')
 const router = Router()
 const _ = require('underscore')
 
+const products = require('../Productos.json')
+
 router.get('/producto', (request, response) => {
-    response.json({"s":"s"})
+    let array_result = []
+    _.each(products, (product, i) => {
+        let sku = product.sku
+        let nombre = product.nombre
+        let url = product.url
+        let marca = product.marca
+        let inventario = product.inventario
+
+        let precio = product.precio
+        let descuento = product.descuento
+        let iva = product.iva
+
+        let precio_final = precio - (precio * descuento) + (precio * iva)
+
+        if(inventario > 0){
+            const showProductsM = { sku, nombre, url, marca, precio_final}
+            array_result.push(showProductsM) 
+        }
+    })
+    response.json(array_result)
+
 })
 
 module.exports = router
