657987b8bd37de5c522188c35b9aef5f2d81b773
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-05 15:18:30 -0500

    Finished

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,17 +11,17 @@ router.delete('/producto/:sku', (request, response) => {
     _.each(products, (product, i) => {
         if(product.sku == sku){
             products.splice(i, 1)
+
+            const delete_process = JSON.stringify(products)
+            fs.writeFileSync('src/Productos.json', delete_process, 'utf-8', (error) => {
+                if(error){
+                    console.log(`Error: ${error}`)
+                }
+            })
+            
+            response.json('Product successfully removed')
         }
     })
-
-    const delete_process = JSON.stringify(products)
-    fs.writeFileSync('src/Productos.json', delete_process, (error) => {
-        if(error){
-            console.log(`Error: ${error}`)
-        }
-    })
-
-    response.json('Product successfully removed')
 
 })
 
